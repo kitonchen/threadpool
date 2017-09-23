4a560321652d017c77f4f28952f7642249d8a223
kiton_Chen <chenpidaxia@gmail.com>
2017-09-23 23:20:58 +0800

    增加了一个 1000000的线程性能测试demo，修改了下pool的逻辑不保证task不被打断
    Signed-off-by: kiton_Chen <chenpidaxia@gmail.com>

--- ThreadPool.h
+++ ThreadPool.h
@@ -11,27 +11,41 @@
 
 using namespace std;
 namespace kiton{
-    const unsigned int MAX_THREAD_NUM=16;
-    class ThreadPool{
-    private://val
+
+    class noncopy{//禁止拷贝和赋值拷贝
+    protected:
+        noncopy()= default;
+        ~noncopy() = default;
+        noncopy(noncopy&) = delete;
+        noncopy&operator=(noncopy&) = delete;
+    };
+    //默认线程数取，机器的线程数
+    const unsigned int DEFAULT_THREAD_NUM=thread::hardware_concurrency();
+    class ThreadPool: protected noncopy{
         using Task =function<void(void)>;
-        vector<thread> _pool;//线程池，线程池里面放的是消费者，主线程是生产者
+    public:
+        enum RUN_MODE{AT_ONCE=1,DELAY=0};//工作模式：立刻生产，延迟生产。。。
+    private://val
+        atomic<short> _mode;//线程池运行模式
+        vector<thread> _pool;//线程池，线程池里面放的是生产者，主线程是消费者
         queue<Task> _task_que;//任务队列
         atomic_bool  _stop_add_task;//是否允许生产任务
         atomic_int  _free_thread_num;//线程数
         mutex       _lock;//互斥锁
         condition_variable _cv_queue_empty;//任务就绪变量
-    private://fun
-            void init_pool(int thread_num);
-    public://val
+
     public://fun
         //返回空闲线程数
         int free_count(){return _free_thread_num;}
+        //返回池的大小
+        int size(){return _pool.size();}
+        //启动池工作
+        bool start();
         ~ThreadPool();
         //初始化线程池初始数量，并且允许线程池工作
-        ThreadPool(int thread_num=MAX_THREAD_NUM):
-        _stop_add_task(false),_free_thread_num(0){
-                init_pool(thread_num);
+        ThreadPool(short mode=RUN_MODE::AT_ONCE,int thread_num=DEFAULT_THREAD_NUM):
+                _stop_add_task(false),_free_thread_num(0),_mode(mode){
+            init_pool(thread_num);
         }
         //任务提交，消费者线程调用
         template<class Fun,class...Args>
@@ -53,14 +67,18 @@ namespace kiton{
                 });//任务队列里面放入一个执行void(void)匿名函数，该匿名函数执行(*task)()，
                 // (*task)()里面才真正执行我们的任务,C++你赢了...
             }
-            _cv_queue_empty.notify_one();//唤醒一个线程进行生产
-            return res;//返回结果
+            if(_mode==RUN_MODE::AT_ONCE){
+                _cv_queue_empty.notify_one();//唤醒一个线程进行生产
+            }
+            return res;//返回该任务的future
         };
 
+    private://fun
+            void init_pool(int thread_num);
     };
     //初始化池
     void ThreadPool::init_pool(int thread_num){
-        while(_pool.size()<MAX_THREAD_NUM&&(--thread_num)>0)
+        while((--thread_num)>=0)
         {
             function<void(void)> producer=[this]{
                 //如果没有往线程池里面添加任务，或者任务队列非空
@@ -73,15 +91,15 @@ namespace kiton{
                         _cv_queue_empty.wait(locker,[this]{return !_task_que.empty();});
                         //取出一个任务
                         //再次检查，走出来后如果关闭提交任务了或者任务队列空了，就返回
-                        if(_task_que.empty()&&_stop_add_task) return ;
+                        if(_task_que.empty()&&_stop_add_task) break;
                         //取出一个任务
                         task = std::move(_task_que.front());
                         _task_que.pop();
                         //调整空闲线程数
-                        _free_thread_num--;
-                        task();//执行任务
-                        _free_thread_num++;
                     }
+                    _free_thread_num--;
+                    task();//执行任务
+                    _free_thread_num++;
                 }
             };
             //把一个生产者线程添加至，线程池里面
@@ -91,15 +109,33 @@ namespace kiton{
         }
     }
 
+    bool ThreadPool::start()
+    {/*
+       线程池开足马力工作
+       以后多线程add_task场景下，可能出现return之前被添加任务，
+       导致任务队列还有任务没进行生产的情况。
+       所以池开始工作后，需要切换成每添加一个任务就立即唤醒一个线程生产工作模式
+       return处时插入的新任务会被立刻执行*/
+        _mode=RUN_MODE ::AT_ONCE;
+        _cv_queue_empty.notify_all();//唤醒所有线程
+        return _task_que.empty();
+    }
+
     ThreadPool::~ThreadPool()
     {
         _stop_add_task=true;
-        _cv_queue_empty.notify_all();//唤醒所有线程
-        for(auto& thread:_pool)
-        {
-            if(thread.joinable()) thread.join();
-        }
 
+        if(_mode==RUN_MODE::AT_ONCE&&_task_que.empty()){
+            for(auto& thread:_pool) {
+                thread.detach();
+            }
+        }//立刻生产则，等待
+        else if(_mode ==RUN_MODE::DELAY){
+            _cv_queue_empty.notify_all();//唤醒所有线程
+            for(auto& thread:_pool) {
+                if(thread.joinable()) thread.join();
+            }
+        }
     }
 }
 
--- main.cpp
+++ main.cpp
@@ -1,30 +1,39 @@
 #include<iostream>
 #include"ThreadPool.h"
 #include<set>
-
+using namespace kiton;
 int main(int argv,char** argc)
 {
-    kiton::ThreadPool threadPool;
+  
     vector<future<void>>  result;
     set<thread::id> thread_id;
-    for(int i=0;i<100;i++)
-    {
-        thread_id.insert(this_thread::get_id());
-        result.emplace_back(
-                threadPool.add_task([i,&thread_id]{cout<<this_thread::get_id()<<" task result:"<<i<<endl;
-                                       })
-        );
-        result.emplace_back(
-                threadPool.add_task([i,&thread_id]{
-                    thread_id.insert(this_thread::get_id());
-                    cout<<"thread id:"<<this_thread::get_id()<<endl;
-                })
-        );
-    }
+    mutex lock;
+    ThreadPool threadPool(ThreadPool::RUN_MODE::AT_ONCE,4);
+        for (long long i = 0; i < 1000000; i++) {
+            result.emplace_back(
+                    threadPool.add_task([i, &thread_id,&lock] {
+                        {
+                            lock_guard<mutex> locker{lock};
+                            thread_id.insert(this_thread::get_id());
+                            cout <<"thread_id: "<< this_thread::get_id() << " run task result:" << i << endl;
+                        }
+                    })
+            );
+        }
+   // threadPool.start();
 
     for(auto& res:result){
         res.get();
     }
-    cout<<endl<<thread_id.size()<<endl;
-    return 0;
+
+    cout<<"池子的大小:"<<threadPool.size()<<endl;
+    cout<<"空闲线程数:"<<threadPool.free_count()<<endl;
+    cout<<"活跃线程数:"<<thread_id.size()<<endl;
+   /*
+    cout<<"活跃线程:"<<endl;
+    for(auto& tid:thread_id){
+        cout<<tid<<endl;
+    }
+    */
+    return EXIT_SUCCESS;
 }
